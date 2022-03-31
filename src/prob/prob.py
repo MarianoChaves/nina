2100d8a774e4f49550378398ad43cfce14c620e0
rafaelarossi <72165515+rafaelarossi@users.noreply.github.com>
2022-03-31 18:57:51 -0300

    Add files via upload
    prob 3 neutrinos

--- src/prob/prob.py
+++ src/prob/prob.py
@@ -1,7 +1,7 @@
 class Prob():
     '''Recebe params (lista de parametros), func (funcao de probabilidade)'''
     def __init__(self, params, func):
-        self.parms = params
+        self.params = params
         self.func = func
     def calculate(self, energy):
         return self.func(energy)
